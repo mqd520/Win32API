8a06eb933e16dfefce219c8e3f34fe8fb00d29cc
Leon520 <1506816370@qq.com>
2016-09-12 19:12:14 +0800

    vbhnbvnnbvnvbnvn

--- Win32API_Test/Form1.cs
+++ Win32API_Test/Form1.cs
@@ -24,7 +24,7 @@ namespace Win32API_Test
         {
             if (m.Msg == 0x010A)
             {
-                Console.WriteLine("0x00A");
+                Console.WriteLine("0xbtyrh46hghtfhfhnjghgng00A");
                 //Win32API.DefWindowProc((IntPtr)m.Msg, (uint)m.Msg, (int)m.WParam, (int)m.LParam);
                 base.WndProc(ref m);
             }
